c9a0db793d92f3a6ed5ef3673c3f77901b31aaf2
kekov <carlsenfischer+git@gmail.com>
2019-06-02 19:41:20 +0200

    Import initial

---
+++ getPlayList.sh
@@ -0,0 +1,1 @@
+youtube-dl  --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" https://music.youtube.com/watch?v=DFsCU8ckap4&list=RDAMVMDFsCU8ckap4
---
+++ getUrlPlaylist.sh
@@ -0,0 +1,1 @@
+youtube-dl 'https://www.youtube.com/playlist?list=LLopMdgxKWL-j_M6HEY0c8yw' --flat-playlist --dump-single-json  | jq
